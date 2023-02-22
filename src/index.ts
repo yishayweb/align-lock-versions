d940a0cdfaa1e51010567f084209f90fca8f38ed
yishayweb <yishayweb@gmail.com>
2023-02-22 10:56:31 +0200

    updated action with scope name input, dependency type map, statuscheck if lock file changed

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,42 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 import { exec, getExecOutput } from "@actions/exec";
 import { getPackages } from "@manypkg/get-packages";
 import { getDependentsGraph } from "./utils/getDependentsGraph";
 
-const changelogRegex = /packages\/([A-Za-z0-9]+)\/CHANGELOG\.md$/;
-const packageNameWithPrefixRegex = /^@yishay20\/([A-Za-z0-9]+)$/;
+const dependencyTypeMap = {
+  dependencies: "",
+  devDependencies: "--save-dev",
+  peerDependencies: "--save-peer",
+};
 
-export const getDiff = async () => {
+export const getFilesDiffInPrBranch = async () => {
   const diff = await getExecOutput("git", [
     "diff",
     "--name-only",
     "origin/changeset-release/master..master",
   ]);
 
+  // this array includes the names of the files changed in the version branch compared to the master branch
   const filesArray = diff.stdout.split("\n");
 
   return filesArray;
 };
 
 export const runPackageManagerVersionUpdate = async (
-  dependentPackageToUpdate,
-  dependencyName
+  dependentPackageToUpdate: string,
+  dependencyName: string,
+  depType: string
 ) => {
   // run pnpm command
-  // pnpm --filter ${dependentName} add ${packageNameWithPrefix} --save-peer
+  // pnpm --filter ${dependentPackageToUpdate} add ${dependencyName} ${dependencyTypeCommand}
+  const dependencyTypeCommand = dependencyTypeMap[depType];
   await exec("pnpm", [
     "--filter",
     dependentPackageToUpdate,
     "add",
     dependencyName,
-    "--save-peer",
+    dependencyTypeCommand,
   ]);
 };
 
@@ -37,11 +44,17 @@ export const updateVersions = async (
   diffFilesArray: string[],
   dependentsMap: Map<string, { dependent: string; depType: string }[]>
 ) => {
+  const packageScopeName = core.getInput("packageScopeName");
+  const changelogRegex = /packages\/([A-Za-z0-9]+)\/CHANGELOG\.md$/;
+  const packageNameWithPrefixRegex = new RegExp(
+    `^@${packageScopeName}\/([A-Za-z0-9]+)$`
+  );
+
   for (const candidateDependencyChangelogFile of diffFilesArray) {
     const isMatch = candidateDependencyChangelogFile.match(changelogRegex);
     if (isMatch) {
       const dependencyName = isMatch[1];
-      const dependencyNameWithPrefix = `@yishay20/${dependencyName}`;
+      const dependencyNameWithPrefix = `@${packageScopeName}/${dependencyName}`;
 
       const currPackageDependentsArray = dependentsMap.get(
         dependencyNameWithPrefix
@@ -60,7 +73,8 @@ export const updateVersions = async (
               if (fileName === `packages/${dependentName}/CHANGELOG.md`) {
                 await runPackageManagerVersionUpdate(
                   dependentNameWithPrefix,
-                  dependencyNameWithPrefix
+                  dependencyNameWithPrefix,
+                  depType
                 );
               }
             }
@@ -72,19 +86,30 @@ export const updateVersions = async (
 };
 
 (async () => {
-  const diffFilesArray = await getDiff();
+  const diffFilesArray = await getFilesDiffInPrBranch();
   const packagesObj = await getPackages(process.cwd());
   const dependents = getDependentsGraph(packagesObj);
   console.log("dependents in:", dependents);
 
   await updateVersions(diffFilesArray, dependents);
 
-  const branchAfter = await getExecOutput("git", ["status"]);
-  console.log("git branch after: ", branchAfter);
+  const branchStatus = await getExecOutput("git", ["status"]);
+  console.log("git branch after: ", branchStatus);
 
-  await exec("git", ["add", "pnpm-lock.yaml"]);
-  await exec("git", ["commit", "-m", "update lock file automatically"]);
-  await exec("git", ["push", "origin", "changeset-release/master"]);
+  const modifiedLockFileRegex = /modified:\s+pnpm-lock.yaml/;
+  const isModifiedLockFileMatched = branchStatus?.stdout?.match(
+    modifiedLockFileRegex
+  );
+
+  if (isModifiedLockFileMatched) {
+    console.log("commiting updated lock file to the pr");
+    await exec("git", ["add", "pnpm-lock.yaml"]);
+    await exec("git", ["commit", "-m", "update lock file automatically"]);
+    await exec("git", ["push", "origin", "changeset-release/master"]);
+  }
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 })().catch((err) => {
   console.error(err);
   core.setFailed(err.message);
