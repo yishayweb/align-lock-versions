0dcd55c48a55b14a00422e805643672a7e0ee875
yishayweb <yishayweb@gmail.com>
2025-07-18 14:23:17 +0300

    updated branch name to be dynamic variable

--- src/utils/commitLockFileIfNeeded.ts
+++ src/utils/commitLockFileIfNeeded.ts
@@ -1,4 +1,5 @@
 import { exec, getExecOutput } from "@actions/exec";
+import * as github from "@actions/github";
 
 export const commitLockFileIfNeeded = async () => {
   const branchStatus = await getExecOutput("git", ["status"]);
@@ -10,12 +11,18 @@ export const commitLockFileIfNeeded = async () => {
 
   if (isModifiedLockFileMatched) {
     console.log("commiting updated lock file to the pr");
+
+    const extendedBranchName = github.context.ref;
+    const branchName = extendedBranchName.replace("refs/heads/", "");
+    console.log("the branch name: ", github.context.ref);
+    console.log("branch name: ", branchName);
+
     await exec("git", ["add", "pnpm-lock.yaml"]);
     await exec("git", [
       "commit",
       "-m",
       "chore: update lock file automatically",
     ]);
-    await exec("git", ["push", "origin", "changeset-release/master"]);
+    await exec("git", ["push", "origin", `changeset-release/${branchName}`]);
   }
 };
--- src/utils/getFilesDiffInPrBranch.ts
+++ src/utils/getFilesDiffInPrBranch.ts
@@ -2,12 +2,15 @@ import { getExecOutput } from "@actions/exec";
 import * as github from "@actions/github";
 
 export const getFilesDiffInPrBranch = async () => {
+  const extendedBranchName = github.context.ref;
+  const branchName = extendedBranchName.replace("refs/heads/", "");
+  console.log("the branch name: ", github.context.ref);
+  console.log("branch name: ", branchName);
   const diff = await getExecOutput("git", [
     "diff",
     "--name-only",
-    "origin/changeset-release/master..master",
+    `origin/changeset-release/${branchName}..${branchName}`,
   ]);
-  console.log("the branch name: ", github.context.ref);
 
   // this array includes the names of the files changed in the version branch compared to the master branch
   const filesArray = diff.stdout.split("\n");
